bab66d11d804b040ea024dd7dc02ce57a8f2b87a
Trou Macacq <macacq@gmail.com>
2018-11-10 11:10:07 +0100

    007 - Unit tests were added

---
+++ LeetCode.Tests/Task007Tests.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using LeetCode.Easy;
+    using Xunit;
+
+    public class Task007Tests
+    {
+        [Theory]
+        [InlineData(123, 321)]
+        [InlineData(-123, -321)]
+        [InlineData(120, 21)]
+        public void CanReverse(int input, int expected)
+        {
+            // Arrange
+            // Act
+            var result = new Task007().Reverse(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ LeetCode/Easy/Task007.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Easy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Given a 32-bit signed integer, reverse digits of an integer.
+    /// 
+    /// Example 1:
+    /// Input: 123
+    /// Output: 321
+    ///
+    /// Example 2:
+    /// Input: -123
+    /// Output: -321
+    /// 
+    /// Example 3:
+    /// Input: 120
+    /// Output: 21
+    /// 
+    /// Note:
+    /// Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. 
+    /// For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+    /// </summary>
+    public class Task007
+    {
+        public int Reverse(int x)
+        {
+            return default(int);
+        }
+    }
+}
