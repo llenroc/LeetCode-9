5536a83f2ed26984f2114b68ed37297de58f0e3e
Trou Macacq <macacq@gmail.com>
2018-11-10 19:56:53 +0100

    007 - 48 ms

--- LeetCode/Easy/Task007.cs
+++ LeetCode/Easy/Task007.cs
@@ -1,14 +1,10 @@
-﻿namespace LeetCode.Easy
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿using System;
 
+namespace LeetCode.Easy
+{
     /// <summary>
     /// Given a 32-bit signed integer, reverse digits of an integer.
-    /// 
+    ///
     /// Example 1:
     /// Input: 123
     /// Output: 321
@@ -16,41 +12,40 @@
     /// Example 2:
     /// Input: -123
     /// Output: -321
-    /// 
+    ///
     /// Example 3:
     /// Input: 120
     /// Output: 21
-    /// 
+    ///
     /// Note:
-    /// Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1]. 
+    /// Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1].
     /// For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
     /// </summary>
     public class Task007
     {
+        #region Public Methods
+
         public int Reverse(int x)
         {
-            var negative = default(bool);
-            var y = x.ToString();
-            if (y[0] == '-')
-            {
-                negative = true;
-                y = y.Substring(1);
-            }
-            var z = y.Reverse();
-
             var result = default(int);
+            var part = default(long);
 
-            try
+            while (x != 0)
             {
-                result = int.Parse(new string(z.ToArray()));
-            }
-            catch
-            {
+                var digit = x % 10;
+
+                part = (long)result * 10 + digit;
+
+                result = part > int.MaxValue || part < int.MinValue 
+                    ? 0
+                    : (int)part;
+
+                x = (x - digit) / 10;
             }
 
-            return negative
-                ? -result
-                : result;
+            return result;
         }
+
+        #endregion Public Methods
     }
 }
