c1d61ec86d53999c9ddc1eab5cfbd28b26c3128a
Trou Macacq <macacq@gmail.com>
2018-11-11 21:33:31 +0100

    013 - 116 ms

--- LeetCode/Easy/Task013.cs
+++ LeetCode/Easy/Task013.cs
@@ -1,14 +1,8 @@
 ﻿namespace LeetCode.Easy
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
     /// <summary>
     /// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-    /// 
+    ///
     /// Symbol Value
     /// I             1
     /// V             5
@@ -18,31 +12,31 @@
     /// D             500
     /// M             1000
     /// For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
-    /// 
+    ///
     /// Roman numerals are usually written largest to smallest from left to right.However, the numeral for four is not IIII. Instead, the number four is written as IV.Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX.There are six instances where subtraction is used:
-    /// 
-    /// I can be placed before V (5) and X(10) to make 4 and 9. 
-    /// X can be placed before L(50) and C(100) to make 40 and 90. 
+    ///
+    /// I can be placed before V (5) and X(10) to make 4 and 9.
+    /// X can be placed before L(50) and C(100) to make 40 and 90.
     /// C can be placed before D(500) and M(1000) to make 400 and 900.
     /// Given a roman numeral, convert it to an integer.Input is guaranteed to be within the range from 1 to 3999.
-    /// 
+    ///
     /// Example 1:
     /// Input: "III"
     /// Output: 3
-    /// 
+    ///
     /// Example 2:
     /// Input: "IV"
     /// Output: 4
-    /// 
+    ///
     /// Example 3:
     /// Input: "IX"
     /// Output: 9
-    /// 
+    ///
     /// Example 4:
     /// Input: "LVIII"
     /// Output: 58
     /// Explanation: L = 50, V= 5, III = 3.
-    /// 
+    ///
     /// Example 5:
     /// Input: "MCMXCIV"
     /// Output: 1994
@@ -50,9 +44,58 @@
     /// </summary>
     public class Task013
     {
+        #region Public Methods
+
         public int RomanToInt(string s)
         {
-            return default(int);
+            var digit = default(int);
+            var result = default(int);
+
+            for (var i = s.Length - 1; i >= 0; i--)
+            {
+                switch (s[i])
+                {
+                    case 'I':
+                        digit = (digit == 5 || digit == 10)
+                            ? -1
+                            : 1;
+                        break;
+
+                    case 'V':
+                        digit = 5;
+                        break;
+
+                    case 'X':
+                        digit = (digit == 50 || digit == 100)
+                            ? -10
+                            : 10;
+                        break;
+
+                    case 'L':
+                        digit = 50;
+                        break;
+
+                    case 'C':
+                        digit = (digit == 500 || digit == 1000)
+                            ? -100
+                            : 100;
+                        break;
+
+                    case 'D':
+                        digit = 500;
+                        break;
+
+                    case 'M':
+                        digit = 1000;
+                        break;
+                }
+
+                result += digit;
+            }
+
+            return result;
         }
+
+        #endregion Public Methods
     }
 }
