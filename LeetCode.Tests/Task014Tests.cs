b07dbde0280765702311b165e2f633b123768710
Trou Macacq <macacq@gmail.com>
2018-11-13 12:39:21 +0100

    014 - Unit tests were added

---
+++ LeetCode.Tests/Task014Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Tests
+{
+    using LeetCode.Easy;
+    using Xunit;
+
+    public class Task014Tests
+    {
+        #region Public Methods
+
+        [Theory]
+        [InlineData(new string[] { "flower", "flow", "flight" }, "fl")]
+        [InlineData(new string[] { "dog", "racecar", "car" }, "")]
+        public void CanDetectLongestCommonPrefix(string[] input, string expected)
+        {
+            // Arrange
+            // Act
+            var result = new Task014().LongestCommonPrefix(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ LeetCode/Easy/Task014.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Easy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Write a function to find the longest common prefix string amongst an array of strings.
+    /// 
+    /// If there is no common prefix, return an empty string "".
+    /// 
+    /// Example 1:
+    /// Input: ["flower","flow","flight"]
+    /// Output: "fl"
+    /// 
+    /// Example 2:
+    /// Input: ["dog","racecar","car"]
+    /// Output: ""
+    /// Explanation: There is no common prefix among the input strings.
+    /// 
+    /// Note:
+    /// All given inputs are in lowercase letters a-z.
+    /// </summary>
+    public class Task014
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            return default(string);
+        }
+    }
+}
