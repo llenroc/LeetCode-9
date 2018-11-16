6cec2453918fc985685831184190de70ab3d5d30
Trou Macacq <macacq@gmail.com>
2018-11-16 16:00:35 +0100

    020 - Unit tests were added

---
+++ LeetCode.Tests/Task020Tests.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Tests
+{
+    using LeetCode.Easy;
+    using Xunit;
+
+    public class Task020Tests
+    {
+        #region Public Methods
+
+        [Theory]
+        [InlineData("()", true)]
+        [InlineData("()[]{}", true)]
+        [InlineData("(]", false)]
+        [InlineData("([)]", false)]
+        [InlineData("{[]}", true)]
+        public void CanDetectIsValid(string input, bool expected)
+        {
+            // Arrange
+            // Act
+            var result = new Task020().IsValid(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        #endregion Public Methods
+    }
+}
