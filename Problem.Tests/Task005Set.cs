fd7129b2505ec2d5a01dd20c9458c04580095a63
Trou Macacq <macacq@gmail.com>
2018-11-23 13:15:10 +0100

    005 - Unit tests were added

---
+++ Problem.Tests/Task005Set.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problem.Tests
+{
+    using LeetCode.Problem.Medium;
+    using Xunit;
+
+    public class Task005Set
+    {
+        #region Public Methods
+
+        [Theory]
+        [Trait("Difficulty", "Medium")]
+        [Trait("Topic", "String")]
+        [Trait("Topic", "Dynamic Programming")]
+        [InlineData("babad", "bab")]
+        [InlineData("babad", "aba")]
+        [InlineData("cbbd", "bb")]
+        public void CanFindLongestPalindrome(string input, string expected)
+        {
+            // Arrange
+            // Act
+            var result = new Task005().LongestPalindrome(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        #endregion Public Methods
+    }
+}
