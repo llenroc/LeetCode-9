bebe9795cd327def2b3359760441e8499fb90d63
Trou Macacq <macacq@gmail.com>
2019-10-28 19:13:55 +0100

    038 - 84 ms

---
+++ Problem.Tests/Task038Set.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Problem.Tests
+{
+    using LeetCode.Problem.Easy;
+    using Xunit;
+
+    public class Task038Set
+    {
+        #region Public Methods
+
+        [Theory]
+        [Trait("Difficulty", "Easy")]
+        [Trait("Topic", "String")]
+        [InlineData(1, "1")]
+        [InlineData(2, "11")]
+        [InlineData(3, "21")]
+        [InlineData(4, "1211")]
+        [InlineData(5, "111221")]
+        [InlineData(6, "312211")]
+        [InlineData(7, "13112221")]
+        [InlineData(8, "1113213211")]
+        [InlineData(9, "31131211131221")]
+        [InlineData(10, "13211311123113112211")]
+        public void CanCountAndSay(int number, string word)
+        {
+            // Arrange
+            // Act
+            var result = new Task038().CountAndSay(number);
+
+            // Assert
+            Assert.Equal(word, result);
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ Problem/Easy/Task038.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCode.Problem.Easy
+{
+    using System.Text;
+
+    /// <summary>
+    /// The count-and-say sequence is the sequence of integers with the first five terms as following:
+    ///
+    /// 1.     1
+    /// 2.     11
+    /// 3.     21
+    /// 4.     1211
+    /// 5.     111221
+    ///
+    /// 1 is read off as "one 1" or 11.
+    /// 11 is read off as "two 1s" or 21.
+    /// 21 is read off as "one 2, then one 1" or 1211.
+    ///
+    /// Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+    ///
+    /// Note: Each term of the sequence of integers will be represented as a string.
+    /// </summary>
+    public class Task038
+    {
+        #region Public Methods
+
+        public string CountAndSay(int n)
+        {
+            var result = "1";
+            var builder = new StringBuilder();
+
+            for (var i = 1; i < n; i++)
+            {
+                var start = result[0];
+                var count = 1;
+
+                for (var j = 1; j < result.Length; j++)
+                {
+                    if (start == result[j])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        builder.Append(count);
+                        builder.Append(start);
+                        start = result[j];
+                        count = 1;
+                    }
+                }
+
+                builder.Append(count);
+                builder.Append(start);
+
+                result = builder.ToString();
+            }
+
+            return result;
+        }
+
+        #endregion Public Methods
+    }
+}
