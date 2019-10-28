0a70063d1b1d5cd9406ee085123785370c906629
Trou Macacq <macacq@gmail.com>
2019-10-28 19:13:34 +0100

    520 - Cleaning up the code

--- Problem/Easy/Task520.cs
+++ Problem/Easy/Task520.cs
@@ -1,17 +1,14 @@
 namespace LeetCode.Problem.Easy
 {
-    using System;
-    using System.Collections.Generic;
-
     /// <summary>
-    // Given a word, you need to judge whether the usage of capitals in it is right or not.
-    // 
-    // We define the usage of capitals in a word to be right when one of the following cases holds:
-    // 
-    // All letters in this word are capitals, like "USA".
-    // All letters in this word are not capitals, like "leetcode".
-    // Only the first letter in this word is capital, like "Google".
-    // Otherwise, we define that this word doesn't use capitals in a right way.
+    /// Given a word, you need to judge whether the usage of capitals in it is right or not.
+    ///
+    /// We define the usage of capitals in a word to be right when one of the following cases holds:
+    ///
+    /// All letters in this word are capitals, like "USA".
+    /// All letters in this word are not capitals, like "leetcode".
+    /// Only the first letter in this word is capital, like "Google".
+    /// Otherwise, we define that this word doesn't use capitals in a right way.
     /// </summary>
     public class Task520
     {
